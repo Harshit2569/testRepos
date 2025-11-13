6b62f5f9d104b0edbda92cf2cb27604b1e5bde8b
Harshit <cr273916@gmail.com>
2025-11-13 16:05:13 +0530

    Initial React project setup

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <div id="nav">
+        <h2>Harshit</h2>
+        <div id="list">
+          <h4>About</h4>
+          <h4>Work</h4>
+          <h4>Career</h4>
+          <h4>Contact</h4>
+        </div>
+      </div>
+      <h1 className="name">Harshit Upadhyay</h1>
+    </div>
+  );
+}
+
+export default Home;
+
